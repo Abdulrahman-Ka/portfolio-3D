a4f37a5164a911ee8b425e51ed9786670b48e901
Abdulrahman_Ka <abode.kalls@gmail.com>
2025-12-28 09:08:24 +0300

    adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import Navbar from "./components/NavBar";
 import Hero from "./components/sections/Hero";
 import ShowcaseSection from "./components/sections/ShowcaseSection";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Hero />
-      <ShowcaseSection/>
+      <ShowcaseSection />
     </>
   );
 };
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 150;
+      setScrolled(isScrolled);
+    };
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
+      <div className="inner">
+        <a href="#hero" className="logo">
+          AbdulRahman | KA
+        </a>
+        <nav className="desktop">
+          <ul>
+            {navLinks.map(({ link, name }, index) => (
+              <li key={index} className="group">
+                <a href={link}>
+                  <span>{name}</span>
+                  <span className="underline" />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <a href="#contact" className="contact-btn group">
+          <div className="inner">
+            <span>Contact me</span>
+          </div>
+        </a>
+      </div>
+    </header>
+  );
+};
+export default Navbar;
